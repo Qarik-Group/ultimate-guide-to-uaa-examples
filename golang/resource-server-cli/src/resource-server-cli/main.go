6e12351b6e1d87b549b9ca9eafa0117bba737bb0
Dr Nic Williams <drnicwilliams@gmail.com>
2018-07-02 09:23:27 +1000

    extract Token() and reuse

--- golang/resource-server-cli/src/resource-server-cli/main.go
+++ golang/resource-server-cli/src/resource-server-cli/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/cloudfoundry-community/go-uaa"
@@ -13,6 +15,7 @@ var opts struct {
 	AirportsURL string `long:"airports-url" env:"AIRPORTS_URL" default:"https://localhost:9292"`
 	UAAURL      string `long:"uaa-url" env:"UAA_URL" description:"Target UAA URL, e.g. https://login.mycompany.com:8443"`
 	UAACACert   string `long:"uaa-ca-cert" env:"UAA_CA_CERT"`
+	UAAZoneID   string `kong:"uaa-zone-id" env:"UAA_ZONE_ID"`
 	Username    string `short:"u" long:"username" env:"UAA_USERNAME" description:"Username for authenticated user"`
 	Password    string `short:"p" long:"password" env:"UAA_PASSWORD" description:"Password for authenticated user"`
 	Verbose     bool   `short:"v" long:"verbose" description:"Show verbose debug information" env:"UAA_TRACE"`
@@ -28,18 +31,39 @@ func main() {
 		os.Exit(1)
 	}
 
-	api, err := uaa.NewWithPasswordCredentials(opts.UAAURL, "", UAAClient, UAAClientSecret, opts.Username, opts.Password, uaa.JSONWebToken)
+	creds, err := uaa.NewWithPasswordCredentials(opts.UAAURL, opts.UAAZoneID, UAAClient, UAAClientSecret, opts.Username, opts.Password, uaa.JSONWebToken)
 	if err != nil {
 		log.Fatal(err)
 	}
+	creds.API.Verbose = opts.Verbose
+	if opts.UAACACert != "" {
+		creds.API.SkipSSLValidation = true
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		creds.API.UnauthenticatedClient = &http.Client{Transport: tr}
+	}
+
+	token, err := creds.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// To save to .user.json later
+	// s, err := json.Marshal(token)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(string(s))
+
+	api, err := uaa.NewWithToken(opts.UAAURL, opts.UAAZoneID, *token)
 	api.Verbose = opts.Verbose
 	if opts.UAACACert != "" {
 		api.SkipSSLValidation = true
 	}
-
-	userinfo, err := api.GetMe()
+	me, err := api.GetMe()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%#v\n", userinfo)
+	fmt.Printf("%#v\n", me)
 }
