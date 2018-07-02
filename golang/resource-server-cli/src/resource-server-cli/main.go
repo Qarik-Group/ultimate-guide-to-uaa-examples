df80e8028958d26606c58ade7de1db2ba4d2620a
Dr Nic Williams <drnicwilliams@gmail.com>
2018-07-02 10:35:01 +1000

    store + reuse access token in .user.json

--- golang/resource-server-cli/src/resource-server-cli/main.go
+++ golang/resource-server-cli/src/resource-server-cli/main.go
@@ -10,18 +10,21 @@ import (
 	"net/http"
 	"os"
 
+	"golang.org/x/oauth2"
+
 	"github.com/cloudfoundry-community/go-uaa"
 	"github.com/jessevdk/go-flags"
 )
 
 var opts struct {
-	AirportsURL string `long:"airports-url" env:"AIRPORTS_URL" default:"http://localhost:9292"`
-	UAAURL      string `long:"uaa-url" env:"UAA_URL" description:"Target UAA URL, e.g. https://login.mycompany.com:8443"`
-	UAACACert   string `long:"uaa-ca-cert" env:"UAA_CA_CERT"`
-	UAAZoneID   string `long:"uaa-zone-id" env:"UAA_ZONE_ID"`
-	Username    string `short:"u" long:"username" env:"UAA_USERNAME" description:"Username for authenticated user"`
-	Password    string `short:"p" long:"password" env:"UAA_PASSWORD" description:"Password for authenticated user"`
-	Verbose     bool   `short:"v" long:"verbose" description:"Show verbose debug information" env:"UAA_TRACE"`
+	AirportsURL    string `long:"airports-url" env:"AIRPORTS_URL" default:"http://localhost:9292"`
+	UAAURL         string `long:"uaa-url" env:"UAA_URL" description:"Target UAA URL, e.g. https://login.mycompany.com:8443"`
+	UAACACert      string `long:"uaa-ca-cert" env:"UAA_CA_CERT"`
+	UAAZoneID      string `long:"uaa-zone-id" env:"UAA_ZONE_ID"`
+	Username       string `short:"u" long:"username" env:"UAA_USERNAME" description:"Username for authenticated user"`
+	Password       string `short:"p" long:"password" env:"UAA_PASSWORD" description:"Password for authenticated user"`
+	UserConfigPath string `long:"user-config" env:"AIRPORTS_USER_CONFIG" description:"Path to user auth config file" default:".user.json"`
+	Verbose        bool   `short:"v" long:"verbose" description:"Show verbose debug information" env:"UAA_TRACE"`
 }
 
 type airport struct {
@@ -51,38 +54,50 @@ func main() {
 		os.Exit(1)
 	}
 
-	creds, err := uaa.NewWithPasswordCredentials(opts.UAAURL, opts.UAAZoneID, UAAClient, UAAClientSecret, opts.Username, opts.Password, uaa.JSONWebToken)
+	if _, err := os.Stat(opts.UserConfigPath); os.IsNotExist(err) {
+		creds, err := uaa.NewWithPasswordCredentials(opts.UAAURL, opts.UAAZoneID, UAAClient, UAAClientSecret, opts.Username, opts.Password, uaa.JSONWebToken)
+		if err != nil {
+			log.Fatal(err)
+		}
+		creds.API.Verbose = opts.Verbose
+		if opts.UAACACert != "" {
+			rootCAs, _ := x509.SystemCertPool()
+			if rootCAs == nil {
+				rootCAs = x509.NewCertPool()
+			}
+			rootCAs.AppendCertsFromPEM([]byte(opts.UAACACert))
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{
+					RootCAs: rootCAs,
+				},
+			}
+			creds.API.UnauthenticatedClient = &http.Client{Transport: tr}
+		}
+
+		token, err := creds.Token()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// To save to .user.json later
+		tokenData, err := json.Marshal(token)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile(opts.UserConfigPath, tokenData, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	var token oauth2.Token
+	storedToken, err := ioutil.ReadFile(opts.UserConfigPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	creds.API.Verbose = opts.Verbose
-	if opts.UAACACert != "" {
-		rootCAs, _ := x509.SystemCertPool()
-		if rootCAs == nil {
-			rootCAs = x509.NewCertPool()
-		}
-		rootCAs.AppendCertsFromPEM([]byte(opts.UAACACert))
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: rootCAs,
-			},
-		}
-		creds.API.UnauthenticatedClient = &http.Client{Transport: tr}
-	}
+	json.Unmarshal(storedToken, &token)
 
-	token, err := creds.Token()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// To save to .user.json later
-	// s, err := json.Marshal(token)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(string(s))
-
-	api, err := uaa.NewWithToken(opts.UAAURL, opts.UAAZoneID, *token)
+	api, err := uaa.NewWithToken(opts.UAAURL, opts.UAAZoneID, token)
 	api.Verbose = opts.Verbose
 	if opts.UAACACert != "" {
 		api.SkipSSLValidation = true
@@ -93,7 +108,7 @@ func main() {
 	// }
 	// fmt.Printf("%#v\n", me)
 
-	// Call airports API with authorization
+	// Call airports API with UAA authorization
 	airportClient := &http.Client{Transport: http.DefaultTransport}
 	req, err := http.NewRequest("GET", opts.AirportsURL, nil)
 	req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.Type(), token.AccessToken))
