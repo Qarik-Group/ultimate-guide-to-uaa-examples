3d9294bbb477cba65e7cc6d6cdbe62caa916db08
Dr Nic Williams <drnicwilliams@gmail.com>
2018-07-02 10:06:03 +1000

    use uaa access_token to call Airports API

--- golang/resource-server-cli/src/resource-server-cli/main.go
+++ golang/resource-server-cli/src/resource-server-cli/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,13 +15,30 @@ import (
 )
 
 var opts struct {
-	AirportsURL string `long:"airports-url" env:"AIRPORTS_URL" default:"https://localhost:9292"`
+	AirportsURL string `long:"airports-url" env:"AIRPORTS_URL" default:"http://localhost:9292"`
 	UAAURL      string `long:"uaa-url" env:"UAA_URL" description:"Target UAA URL, e.g. https://login.mycompany.com:8443"`
 	UAACACert   string `long:"uaa-ca-cert" env:"UAA_CA_CERT"`
 	UAAZoneID   string `long:"uaa-zone-id" env:"UAA_ZONE_ID"`
 	Username    string `short:"u" long:"username" env:"UAA_USERNAME" description:"Username for authenticated user"`
 	Password    string `short:"p" long:"password" env:"UAA_PASSWORD" description:"Password for authenticated user"`
 	Verbose     bool   `short:"v" long:"verbose" description:"Show verbose debug information" env:"UAA_TRACE"`
+}
+
+type airport struct {
+	AirportID          int     `json:"Airport ID"`
+	Name               string  `json:"Name"`
+	City               string  `json:"City"`
+	Country            string  `json:"Country"`
+	IATA               string  `json:"IATA"`
+	ICAO               string  `json:"ICAO"`
+	Latitude           float64 `json:"Latitude"`
+	Longitude          float64 `json:"Longitude"`
+	Altitude           int     `json:"Altitude"`
+	Timezone           float64 `json:"Timezone"`
+	DST                string  `json:"DST"`
+	TzDatabaseTimeZone string  `json:"Tz database time zone"`
+	Type               string  `json:"Type"`
+	Source             string  `json:"Source"`
 }
 
 func main() {
@@ -68,9 +87,30 @@ func main() {
 	if opts.UAACACert != "" {
 		api.SkipSSLValidation = true
 	}
-	me, err := api.GetMe()
+	// me, err := api.GetMe()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("%#v\n", me)
+
+	// Call airports API with authorization
+	airportClient := &http.Client{Transport: http.DefaultTransport}
+	req, err := http.NewRequest("GET", opts.AirportsURL, nil)
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.Type(), token.AccessToken))
+	resp, err := airportClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%#v\n", me)
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var airports []airport
+	err = json.Unmarshal(buf, &airports)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v\n", airports)
 }
