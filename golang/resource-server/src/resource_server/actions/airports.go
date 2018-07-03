fc48c6a1d6d4fd338b32038d1d3e0afd89976013
Dr Nic Williams <drnicwilliams@gmail.com>
2018-07-04 09:22:09 +1000

    use code.cloudfoundry.org/uaa-go-client to verify signing key of auth tokens

--- golang/resource-server/src/resource_server/actions/airports.go
+++ golang/resource-server/src/resource_server/actions/airports.go
@@ -2,11 +2,13 @@ package actions
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"strings"
+	"os"
 
-	jwt "github.com/dgrijalva/jwt-go"
+	"code.cloudfoundry.org/clock"
+	"code.cloudfoundry.org/lager"
+	uaaclient "code.cloudfoundry.org/uaa-go-client"
+	uaaconfig "code.cloudfoundry.org/uaa-go-client/config"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/packr"
 )
@@ -18,6 +20,24 @@ type airport struct {
 	Latitude  float64 `json:"Latitude"`
 	Longitude float64 `json:"Longitude"`
 	Altitude  int     `json:"Altitude"`
+}
+
+var uaa uaaclient.Client
+
+func init() {
+	uaaCfg := &uaaconfig.Config{
+		ClientName:       "airports",
+		ClientSecret:     "airports",
+		UaaEndpoint:      os.Getenv("UAA_URL"),
+		SkipVerification: os.Getenv("UAA_CA_CERT") != "",
+	}
+	logger := lager.NewLogger("airports")
+	// logger.RegisterSink(lager.NewPrettySink(os.Stdout, lager.DEBUG))
+	var err error
+	uaa, err = uaaclient.NewClient(logger, uaaCfg, clock.NewClock())
+	if err != nil {
+		panic(err)
+	}
 }
 
 // AirportsHandler returns a list of Australian airports
@@ -37,15 +57,22 @@ func AirportsHandler(c buffalo.Context) error {
 
 	if c.Request().Header["Authorization"] != nil {
 		authHeader := c.Request().Header["Authorization"][0]
-		tokenString := strings.Split(authHeader, " ")[1]
 		length = 20
 
-		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(""), nil
-		})
-		claims := token.Claims.(jwt.MapClaims)
-		fmt.Printf("%#v\n", claims["scope"])
+		err := uaa.DecodeToken(authHeader, "openid")
+		if err != nil {
+			return err
+		}
+		if err := uaa.DecodeToken(authHeader, "airports.50"); err == nil {
+			length = 50
+		}
+		if err := uaa.DecodeToken(authHeader, "airports.all"); err == nil {
+			length = -1
+		}
 	}
 
+	if length == -1 {
+		return c.Render(200, r.JSON(airports))
+	}
 	return c.Render(200, r.JSON(airports[:length]))
 }
